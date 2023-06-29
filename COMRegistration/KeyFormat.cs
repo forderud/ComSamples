6b6e7374be328dba21061f1a8b87435e7da33245
Fredrik Orderud <fredrik.orderud@ge.com>
2023-06-29 22:24:03 +0200

    Remove unused DllSurrogate.cs to simplify the code.

--- COMRegistration/KeyFormat.cs
+++ COMRegistration/KeyFormat.cs
@@ -3,7 +3,6 @@
     internal static class KeyFormat
     {
         public const string CLSID = @"SOFTWARE\Classes\CLSID\{0:B}";
-        public const string AppID = @"SOFTWARE\Classes\AppID\{0:B}";
 
         public static readonly string LocalServer32 = @$"{CLSID}\LocalServer32";
     }
