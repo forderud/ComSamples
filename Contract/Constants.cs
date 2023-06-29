f3348abb004b66bf1dee1624e66d127d385d93a2
Fredrik Orderud <fredrik.orderud@ge.com>
2023-06-29 23:46:37 +0200

    Rename Server.Contract project to IoTServer.Contract.

--- Contract/Constants.cs
+++ Contract/Constants.cs
@@ -7,6 +7,6 @@ namespace Contract
         public const string IoTAgentClass = "AF080472-F173-4D9D-8BE7-435776617347";
         public static readonly Guid IoTAgentClassGuid = Guid.Parse(IoTAgentClass);
 
-        public const string TypeLibraryName = "Server.Contract.tlb";
+        public const string TypeLibraryName = "IoTServer.Contract.tlb";
     }
 }
