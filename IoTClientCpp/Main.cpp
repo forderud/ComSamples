3ad715c3b8121aa9f50d61d5ea188136a4d2e8ec
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-01 13:54:08 +0200

    Remove CoInitializeEx error handling, since this is more or less guaranteed to succeed.

--- IoTClientCpp/Main.cpp
+++ IoTClientCpp/Main.cpp
@@ -1,19 +1,14 @@
 #include <atlbase.h>
 #include <iostream>
-#include <iomanip>
 
 #include <IoTAgent_h.h>
 #include <IoTAgent_i.c>
 
 int main() {
-    HRESULT hr  = ::CoInitializeEx(0, COINITBASE_MULTITHREADED);
-    if (FAILED(hr)) {
-        std::cout << "CoInitializeEx failure: " << hr << std::endl;
-        return 1;
-    }
+    CoInitializeEx(0, COINITBASE_MULTITHREADED);
 
     CComPtr<IIoTAgent> server;
-    hr = ::CoCreateInstance(CLSID_IoTAgent, nullptr, CLSCTX_LOCAL_SERVER, __uuidof(IIoTAgent), (void **)&server);
+    HRESULT hr = ::CoCreateInstance(CLSID_IoTAgent, nullptr, CLSCTX_LOCAL_SERVER, __uuidof(IIoTAgent), (void **)&server);
     if (FAILED(hr)) {
         std::cout << "CoCreateInstance failure: " << hr << std::endl;
         return 1;
@@ -28,7 +23,7 @@ int main() {
 
     std::cout << "pi = " << pi << std::endl;
 
-    ::CoUninitialize();
+    CoUninitialize();
 
     return 0;
 }
