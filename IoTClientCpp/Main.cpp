3fdbaebe0f6a29ff152d6a99231f5cd69c230e35
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-01 13:51:09 +0200

    Simplify error logging.

--- IoTClientCpp/Main.cpp
+++ IoTClientCpp/Main.cpp
@@ -8,25 +8,25 @@
 int main() {
     HRESULT hr  = ::CoInitializeEx(0, COINITBASE_MULTITHREADED);
     if (FAILED(hr)) {
-        std::cout << "CoInitializeEx failure: " << std::hex << std::showbase << hr << std::endl;
+        std::cout << "CoInitializeEx failure: " << hr << std::endl;
         return 1;
     }
 
     CComPtr<IIoTAgent> server;
     hr = ::CoCreateInstance(CLSID_IoTAgent, nullptr, CLSCTX_LOCAL_SERVER, __uuidof(IIoTAgent), (void **)&server);
     if (FAILED(hr)) {
-        std::cout << "CoCreateInstance failure: " << std::hex << std::showbase << hr << std::endl;
+        std::cout << "CoCreateInstance failure: " << hr << std::endl;
         return 1;
     }
 
     double pi = 0;
     hr = server->ComputePi(&pi);
     if (FAILED(hr)) {
-        std::cout << "Failure: " << std::hex << std::showbase << hr << std::endl;
+        std::cout << "Failure: " << hr << std::endl;
         return 1;
     }
 
-    std::cout << "pi = " << std::setprecision(16) << pi << std::endl;
+    std::cout << "pi = " << pi << std::endl;
 
     ::CoUninitialize();
 
