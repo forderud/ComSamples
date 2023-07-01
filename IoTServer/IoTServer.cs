fce0272c7ac2a318d825dfd0e28ea5f597006a24
Fredrik Orderud <forderud@gmail.com>
2023-07-01 13:23:22 +0200

    Improve server logging.

--- IoTServer/IoTServer.cs
+++ IoTServer/IoTServer.cs
@@ -28,29 +28,24 @@ namespace ComServerExample
             _ = ComSupport.ComTask.Run<object>(System.Threading.ApartmentState.MTA, () => {
                 while (m_active)
                 {
-                    // broadcast message to all clients
-                    Console.WriteLine("Broadcasting message to all clients.");
-                    Message msg = default;
-                    msg.sev = Severity.SeverityInfo;
-                    msg.time = 1.23;
-                    msg.desc = "Hello there!";
-                    msg.color = new byte[3] { 255, 0, 0 };
+                    if (m_clients.Count > 0)
+                    {
+                        // broadcast message to all clients
+                        Console.WriteLine("Broadcasting message to all clients.");
+                        Message msg = default;
+                        msg.sev = Severity.SeverityInfo;
+                        msg.time = 1.23;
+                        msg.desc = "Hello there!";
+                        msg.color = new byte[3] { 255, 0, 0 };
 
-                    for (int i = 0; i < m_clients.Count;) {
-                        IIoTClient client = m_clients[i];
-                        try {
-                            // throws COMException if client is disconnected
-                            client.PushMessage(msg);
+                        BroadcastMessage(msg);
+                    } else
+                    {
+                        Console.WriteLine("No connected clients.");
 
-                            // advance to next index if call doesn't throw
-                            i++;
-                        } catch (COMException ex) {
-                            Console.WriteLine("Disconnecting client after call failure (err: " + ex.ErrorCode.ToString("X2") + ")");
-                            m_clients.RemoveAt(i);
-                        }
                     }
 
-                    Thread.Sleep(2000); // 2 seconds
+                    Thread.Sleep(2000); // wait 2 seconds
                 }
                 return null;
             });
@@ -59,6 +54,28 @@ namespace ComServerExample
         public void Dispose()
         {
             m_active = false;
+        }
+
+        /** Broadcast message to all connected clients. Will disconnect clients on RPC failure. */
+        private void BroadcastMessage(Message msg)
+        {
+            for (int i = 0; i < m_clients.Count;)
+            {
+                IIoTClient client = m_clients[i];
+                try
+                {
+                    // throws COMException if client is disconnected
+                    client.PushMessage(msg);
+
+                    // advance to next index if call doesn't throw
+                    i++;
+                }
+                catch (COMException ex)
+                {
+                    Console.WriteLine("Disconnecting client after call failure (err: " + ex.ErrorCode.ToString("X2") + ")");
+                    m_clients.RemoveAt(i);
+                }
+            }
         }
 
         public double ComputePi()
