bb89b0f3b62d7a67fc428201032939ff8494baee
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-02 23:05:16 +0200

    Simplify client code.

--- IoTClientCs/Program.cs
+++ IoTClientCs/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace IoTClientCs
@@ -19,22 +18,9 @@ namespace IoTClientCs
 
         Program ()
         {
-            // If the COM server is registered as an in-proc server (as is the case when using
-            // a DLL surrogate), activation through the Activator will only use the out-of-proc
-            // server if the client and the registered COM server are not the same bitness.
-            //
-            // Type t = Type.GetTypeFromCLSID(IoTServerClassGuid);
-            // var server = (IIoTAgent)Activator.CreateInstance(t);
-            //
-            // This demo explicitly calls CoCreateInstance with CLSCTX_LOCAL_SERVER to force
-            // usage of the out-of-proc server.
-            object obj;
-            int hr = Ole32.CoCreateInstance(IoTServerClassGuid, IntPtr.Zero, Ole32.CLSCTX_LOCAL_SERVER, typeof(IoTAgent.IIoTAgent).GUID, out obj);
-            if (hr < 0) {
-                Marshal.ThrowExceptionForHR(hr);
-            }
+            Type t = Type.GetTypeFromCLSID(IoTServerClassGuid);
+            var server = (IoTAgent.IIoTAgent)Activator.CreateInstance(t);
 
-            var server = (IoTAgent.IIoTAgent)obj;
             server.Subscribe(this);
 
             double pi = server.ComputePi();
@@ -50,21 +36,6 @@ namespace IoTClientCs
             Console.WriteLine("  desc=" + msg.desc);
             Console.WriteLine("  color=(" + msg.color[0] + ", "+ msg.color[1]+", "+ msg.color[2]+")");
             Console.WriteLine("  data=" + msg.data);
-        }
-
-        private class Ole32
-        {
-            // https://docs.microsoft.com/windows/win32/api/wtypesbase/ne-wtypesbase-clsctx
-            public const int CLSCTX_LOCAL_SERVER = 0x4;
-
-            // https://docs.microsoft.com/windows/win32/api/combaseapi/nf-combaseapi-cocreateinstance
-            [DllImport(nameof(Ole32))]
-            public static extern int CoCreateInstance(
-                [In, MarshalAs(UnmanagedType.LPStruct)] Guid rclsid,
-                IntPtr pUnkOuter,
-                uint dwClsContext,
-                [In, MarshalAs(UnmanagedType.LPStruct)] Guid riid,
-                [MarshalAs(UnmanagedType.IUnknown)] out object ppv);
         }
     }
 }
