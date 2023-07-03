803d956dcd90a9110c7d4bef0571ec097c13285a
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-03 21:38:45 +0200

    Extend C++ client with a callback class, so that it matches the C# project.
    Issue:
    * Server doesn't seem to detect client disconnect.

--- MyClientCpp/Main.cpp
+++ MyClientCpp/Main.cpp
@@ -1,7 +1,46 @@
 #include <atlbase.h>
+#include <atlcom.h>
 #include <iostream>
 
 #include <MyInterfaces.tlh>
+
+
+class MyClient : 
+    public CComObjectRootEx<CComMultiThreadModel>,
+    public CComCoClass<MyClient>, // no registry entries
+    public MyInterfaces::IMyClient {
+public:
+    MyClient() {
+    }
+
+    ~MyClient() {
+    }
+
+    HRESULT raw_PushMessage(MyInterfaces::Message msg) override {
+        std::wcout << L"Received message:\n";
+        std::wcout << L"  sev=" << msg.sev << L"\n";
+        std::wcout << L"  time=" << msg.time << L"\n";
+        std::wcout << L"  value=" << msg.value << L"\n";
+        std::wcout << L"  desc=" << msg.desc << L"\n";
+        std::wcout << L"  color=(" << msg.color[0] << L", " << msg.color[1] << L", " << msg.color[2] << L")\n";
+        std::wcout << L"  data=" << msg.data << L"\n";;
+
+        return S_OK;
+    }
+
+    static CComPtr<MyClient> Create() {
+        // create an object (with ref. count zero)
+        CComObject<MyClient> * tmp = nullptr;
+        CComObject<MyClient>::CreateInstance(&tmp);
+        // move into smart-ptr (will incr. ref. count to one)
+        return tmp;
+    }
+
+    BEGIN_COM_MAP(MyClient)
+        COM_INTERFACE_ENTRY(MyInterfaces::IMyClient)
+    END_COM_MAP()
+};
+
 
 int main() {
     CoInitializeEx(0, COINITBASE_MULTITHREADED);
@@ -14,6 +53,9 @@ int main() {
     }
 
     try {
+        auto callback = MyClient::Create();
+        server->Subscribe(callback);
+
         auto cruncher = server->GetNumberCruncher();
         double pi = cruncher->ComputePi();
         std::wcout << L"pi = " << pi << std::endl;
@@ -22,7 +64,14 @@ int main() {
         return 1;
     }
 
+    // wait 5 seconds before exiting to give the server time to send messages
+    Sleep(5000);
+
     CoUninitialize();
 
     return 0;
 }
+
+// instantiate ATL
+class MyClientModule : public CAtlExeModuleT<MyClientModule> {};
+MyClientModule _Module;
