a932bf4f4a5494c38f6e4ef187a8b52a45af9088
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-03 20:33:27 +0200

    Rename IoTAgent project to MyInterfaces.

--- MyInterfaces/WrapperGen.cpp
+++ MyInterfaces/WrapperGen.cpp
@@ -1,8 +1,8 @@
 // Generate C++ convenience wrapper headers
 #ifdef _WIN64
   #ifdef _DEBUG
-    #import "x64/Debug/IoTAgent.tlb"
+    #import "x64/Debug/MyInterfaces.tlb"
   #else
-    #import "x64/Release/IoTAgent.tlb"
+    #import "x64/Release/MyInterfaces.tlb"
   #endif
 #endif
--- UNREGISTER.bat
+++ UNREGISTER.bat
@@ -11,10 +11,10 @@ for %%P in (32 64) do (
   reg delete "HKCR\Interface\{F586D6F4-AF37-441E-80A6-3D33D977882D}" /f /reg:%%P 2> NUL
 )
 
-:: Delete IoTAgent TypeLib
+:: Delete MyInterfaces TypeLib
 reg delete "HKCR\TypeLib\{46F3FEB2-121D-4830-AA22-0CDA9EA90DC3}" /f 2> NUL
 
-:: Delete IoTSever COM class
+:: Delete MyServer COM class
 reg delete "HKCR\CLSID\{AF080472-F173-4D9D-8BE7-435776617347}" /f 2> NUL
 
 
