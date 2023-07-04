98431d094a05a53e3fa940b5818ff7c0aae7e1d0
Fredrik Orderud <fredrik.orderud@ge.com>
2023-07-04 14:41:12 +0200

    Impl. "data" array printing also in C++ sample.

--- MyClientCpp/Main.cpp
+++ MyClientCpp/Main.cpp
@@ -1,8 +1,25 @@
 #include <atlbase.h>
 #include <atlcom.h>
+#include <atlsafe.h>
 #include <iostream>
-
+#include <vector>
 #include <MyInterfaces.tlh>
+
+
+/** Convert SAFEARRAY to a std::vector> */
+template<class T>
+std::vector<T> ToStdVector(const SAFEARRAY * sa) {
+    CComSafeArray<T> arr;
+
+    arr.Attach(sa);
+    unsigned int size = arr.GetCount();
+    std::vector<T> result(size);
+    for (unsigned int i = 0; i < size; ++i)
+        result[i] = arr[(LONG)i];
+    
+    arr.Detach();
+    return result;
+}
 
 
 class MyClient : 
@@ -23,8 +40,14 @@ public:
         std::wcout << L"  value=" << msg.value << L"\n";
         std::wcout << L"  desc=" << msg.desc << L"\n";
         std::wcout << L"  color=(" << msg.color[0] << L", " << msg.color[1] << L", " << msg.color[2] << L")\n";
-        std::wcout << L"  data=" << msg.data << L"\n";
+        {
+            auto arr = ToStdVector<BYTE>(msg.data);
 
+            std::wcout << L"  data=[";
+            for (BYTE elm : arr)
+                std::wcout << elm << L",";
+            std::wcout << L"]\n";
+        }
         return S_OK;
     }
 
