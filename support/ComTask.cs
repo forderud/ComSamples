623fcc00b0ea0947716f2084ac64e27606557aea
Fredrik Orderud <fredrik.orderud@ge.com>
2023-11-22 12:07:40 +0100

    Add .Net ComTask.Run support function to demonstrate how to combine COM with .Net threads
    The important point here is that SetApartmentState needs to be called _before_ starting the thread, or else COM won't be initialized for the thread.

---
+++ support/ComTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class ComTask
+{
+    /** Task with the thread initialized in a specified COM apartment. */
+    public static Task<T> Run<T>(ApartmentState apartment, Func<T> func)
+    {
+        var tcs = new TaskCompletionSource<T>();
+        Thread thread = new Thread(() => {
+            try
+            {
+                tcs.SetResult(func());
+            }
+            catch (Exception e)
+            {
+                tcs.SetException(e);
+            }
+        });
+        thread.SetApartmentState(apartment);
+        thread.Start();
+        return tcs.Task;
+    }
+}
