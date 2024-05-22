ca7bd79fa1b478133119e3dbd4e76115874e5f11
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2024-05-22 12:11:16 +0200

    Add CoInitializeSecurity signatures from https://www.pinvoke.net/default.aspx/ole32.CoInitializeSecurity

---
+++ ComSupport/ComSecurity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComSupport
+{
+    public enum RpcAuthnLevel
+    {
+        Default = 0,
+        None = 1,
+        Connect = 2,
+        Call = 3,
+        Pkt = 4,
+        PktIntegrity = 5,
+        PktPrivacy = 6
+    }
+
+    public enum RpcImpLevel
+    {
+        Default = 0,
+        Anonymous = 1,
+        Identify = 2,
+        Impersonate = 3,
+        Delegate = 4
+    }
+
+    public enum EoAuthnCap
+    {
+        None = 0x00,
+        MutualAuth = 0x01,
+        StaticCloaking = 0x20,
+        DynamicCloaking = 0x40,
+        AnyAuthority = 0x80,
+        MakeFullSIC = 0x100,
+        Default = 0x800,
+        SecureRefs = 0x02,
+        AccessControl = 0x04,
+        AppID = 0x08,
+        Dynamic = 0x10,
+        RequireFullSIC = 0x200,
+        AutoImpersonate = 0x400,
+        NoCustomMarshal = 0x2000,
+        DisableAAA = 0x1000
+    }
+
+    public class ComSecurity
+    {
+        [System.Runtime.InteropServices.DllImport("ole32.dll")]
+        public static extern int CoInitializeSecurity(IntPtr pVoid, int
+            cAuthSvc, IntPtr asAuthSvc, IntPtr pReserved1, RpcAuthnLevel level,
+            RpcImpLevel impers, IntPtr pAuthList, EoAuthnCap dwCapabilities, IntPtr
+            pReserved3);
+    }
+}
