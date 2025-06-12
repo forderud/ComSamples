9e7a57c46c365897cc30abcf800b914e48c8519e
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-06-12 14:02:38 +0200

    bugfix

--- support/CurrentModule.hpp
+++ support/CurrentModule.hpp
@@ -5,7 +5,7 @@
 inline std::wstring GetModulePath () {
     HMODULE module = nullptr;
     // Get handle to exe/dll that this static lib is linked against
-    auto* module_ptr = (wchar_t const*)GetModuleFolderPath; // pointer to current function
+    auto* module_ptr = (wchar_t const*)GetModulePath; // pointer to current function
     GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, module_ptr, &module);
 
     // retrieve full exe/dll path (incl. filename)
@@ -20,6 +20,6 @@ inline std::wstring GetModuleFolderPath () {
     std::wstring file_path = GetModulePath();
 
     // Get path without filename part
-    PathRemoveFileSpecW(file_path);
+    PathRemoveFileSpecW(file_path.data());
     return file_path;
 }
