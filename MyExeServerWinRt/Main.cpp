e52fbf26f686800b85482c3e0d36f2f9ca5dca50
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-06-13 10:52:38 +0200

    Move WinRtUtils.hpp to shared "support" folder, since it will soon be reused across projects.

--- MyExeServerWinRt/Main.cpp
+++ MyExeServerWinRt/Main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
-#include "WinRtUtils.hpp"
+#include "../support/WinRtUtils.hpp"
 
 #include "MyInterfaces.tlh"
 using namespace MyInterfaces;
