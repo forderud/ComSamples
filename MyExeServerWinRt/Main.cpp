03e58f1c9cfff1664099c9128b9e4fe5b1880c75
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-08-07 10:53:13 +0200

    Update link to WIL register_com_server fix.

--- MyExeServerWinRt/Main.cpp
+++ MyExeServerWinRt/Main.cpp
@@ -40,7 +40,7 @@ int wmain(int argc, wchar_t* argv[]) {
         });
 
     // register class factory in current process
-    // TODO: Replace with wil::register_com_server after https://github.com/microsoft/wil/pull/533 is fixed.
+    // TODO: Replace with wil::register_com_server after https://github.com/microsoft/wil/pull/537 is fixed (version > v1.0.250325.1)
     DWORD registration = 0;
     winrt::check_hresult(::CoRegisterClassObject(__uuidof(MyServer), winrt::make<ClassFactory<MyServerImpl>>().get(), CLSCTX_LOCAL_SERVER, REGCLS_MULTIPLEUSE, &registration));
 
