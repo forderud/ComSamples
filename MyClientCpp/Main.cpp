7acb2b718b5217d89017ccf9a647e748561b82a0
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-08-13 15:34:42 +0200

    Call ResolveOxid to resolve local TCP port from OXID.

--- MyClientCpp/Main.cpp
+++ MyClientCpp/Main.cpp
@@ -24,6 +24,151 @@ std::vector<T> ToStdVector(const SAFEARRAY * sa) {
     return result;
 }
 
+/** Interface Pointer IDentifier (IPID). A 128-bit number that uniquely
+    identifies an interface on an object within an object exporter. */
+#pragma pack(push, 1)
+struct COGETSERVERPID_IPID {
+    unsigned short field1;
+    unsigned short field2;
+    unsigned short pid;    ///< process ID (clamped on overflow)
+    unsigned short field4;
+    unsigned short fields[4];
+
+    bool IsPidValid() const {
+        return pid != 0xFFFF; // detect clamped PID
+    }
+};
+#pragma pack(pop)
+static_assert(sizeof(COGETSERVERPID_IPID) == 16, "COGETSERVERPID_IPID size mismatch");
+
+#pragma pack(push, 1)
+struct COGETSERVERPID_STDOBJREF {
+    unsigned long       flags;       // SORF_ flags (see above)
+    unsigned long       cPublicRefs; // count of references passed
+    unsigned hyper      oxid;        // oxid of server with this oid
+    unsigned hyper      oid;         // oid of object with this ipid
+    COGETSERVERPID_IPID ipid;        // ipid of Interface
+};
+#pragma pack(pop)
+
+/** Partial OBJREF structure up to PID member at offset 52 bytes.
+    Packed struct to make offsets deterministic.
+    REF: https://en.wikipedia.org/wiki/OBJREF */
+#pragma pack(push, 1)
+struct COGETSERVERPID_OBJREF {
+    unsigned long            signature; ///< Should be 'MEOW'
+    unsigned long            flags;     ///< 1 (OBJREF_STANDARD)
+    GUID                     iid;       ///< IID_IUnknown
+    COGETSERVERPID_STDOBJREF std;
+
+    bool IsValid() const {
+        return (signature == 0x574f454d) && (flags == 1);
+    }
+};
+#pragma pack(pop)
+
+
+/** Get the Process ID of a COM server running in a different process. */
+inline HRESULT GetServerSTDOBJREF(IUnknown* obj, /*out*/COGETSERVERPID_STDOBJREF& objref) {
+    if (obj == NULL)
+        return E_INVALIDARG;
+
+    {
+        // IProxyManager proxy type required to assume COGETSERVERPID_OBJREF format
+        CComPtr<IUnknown> proxy_mgr;
+        HRESULT hr = obj->QueryInterface(IID_IProxyManager, (void**)&proxy_mgr);
+        if (FAILED(hr))
+            return hr;
+    }
+
+    // create IStream object
+    CComPtr<IStream> stream;
+    HRESULT hr = ::CreateStreamOnHGlobal(NULL, TRUE/*delete on release*/, &stream);
+    if (FAILED(hr))
+        return hr;
+
+    // marshal "obj" into stream
+    hr = ::CoMarshalInterface(stream, IID_IUnknown, obj, MSHCTX_INPROC, NULL, MSHLFLAGS_NORMAL);
+    if (FAILED(hr))
+        return hr;
+
+    // map stream into memory
+    HGLOBAL hg = NULL;
+    hr = ::GetHGlobalFromStream(stream, &hg);
+    if (SUCCEEDED(hr)) {
+        hr = RPC_E_INVALID_OBJREF; // assume failure until proven othervise
+
+        auto* header = (COGETSERVERPID_OBJREF*)GlobalLock(hg);
+        size_t header_size = GlobalSize(hg);
+        if (header && (header_size >= sizeof(COGETSERVERPID_OBJREF))) {
+            if (header->IsValid()) {
+                objref = header->std;
+                hr = S_OK;
+            }
+
+            GlobalUnlock(hg);
+        }
+    }
+
+    // rewind stream and release marshal data
+    LARGE_INTEGER zero{};
+    stream->Seek(zero, SEEK_SET, NULL);
+    CoReleaseMarshalData(stream);
+
+    return hr;
+}
+
+
+static uint16_t GetTcpPortFromOXID(OXID oxid) {
+    RPC_BINDING_HANDLE rpcConn = 0;
+    {
+        // RPC over TCP/IP MUST use port 135 (https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rpce/95fbfb56-d67a-47df-900c-e263d6031f22)
+        RPC_WSTR rpcConnStr = nullptr;
+        RPC_STATUS status = RpcStringBindingComposeW(nullptr, RPC_WSTR(L"ncacn_ip_tcp"), RPC_WSTR(L"127.0.0.1"), RPC_WSTR(L"135"), NULL, &rpcConnStr);
+        assert(status == RPC_S_OK);
+
+        status = RpcBindingFromStringBindingW(rpcConnStr, &rpcConn);
+        assert(status == RPC_S_OK);
+
+        RpcStringFreeW(&rpcConnStr);
+    }
+
+    // Make OXID Resolver authenticate without a password
+    RPC_STATUS status = RpcBindingSetAuthInfoExW(rpcConn,
+        RPC_WSTR(L"NT Authority\\NetworkService"),
+        RPC_C_AUTHN_LEVEL_PKT_INTEGRITY, // see https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dcom/9b20084f-f673-4486-b81c-f6cdcc5edf84
+        RPC_C_AUTHN_DEFAULT,
+        NULL,
+        RPC_C_AUTHZ_NONE,
+        nullptr
+    );
+    assert(status == RPC_S_OK);
+
+    // Query OXID Resolver for COM server TCP port
+    // This will cause the COM server to start listening on the returned TCP port
+    unsigned short requestedProtocols[] = { 0x07 }; // TCP protocol ID from DCE 1.1: Remote Procedure Call
+    DUALSTRINGARRAY* COMServerStringBindings = nullptr;
+    IPID remoteUnknownIPID{};
+    DWORD authHint = 0;
+    error_status_t err = ResolveOxid(rpcConn, &oxid, _countof(requestedProtocols), requestedProtocols, &COMServerStringBindings, &remoteUnknownIPID, &authHint);
+    assert(err == RPC_S_OK);
+
+    RpcBindingFree(&rpcConn);
+
+    std::wstring_view strBindings((wchar_t*)COMServerStringBindings->aStringArray, COMServerStringBindings->wSecurityOffset);
+    //std::wstring_view secBindings((wchar_t*)COMServerStringBindings->aStringArray + COMServerStringBindings->wSecurityOffset, COMServerStringBindings->wNumEntries - COMServerStringBindings->wSecurityOffset);
+
+    std::wstring localPort; // server TCP port
+    {
+        size_t idx1 = strBindings.find(L"[");
+        size_t idx2 = strBindings.find(L"]", idx1);
+
+        localPort = strBindings.substr(idx1 + 1, idx2 - idx1 - 1);
+    }
+
+    return (uint16_t)std::stoi(localPort);
+}
+
 
 /** Non-creatable COM class that doesn't need any CLSID. */
 class MyClient : 
@@ -93,6 +238,13 @@ int main() {
             _com_error err(hr);
             std::wcout << L"CoCreateInstance failure: " << err.ErrorMessage() << std::endl;
             return 1;
+        }
+
+        {
+            COGETSERVERPID_STDOBJREF objref{};
+            GetServerSTDOBJREF(server, /*out*/objref);
+            uint16_t port = GetTcpPortFromOXID(objref.oxid);
+            std::wcout << L"Server TCP port: " << port << L"\n";
         }
 
         try {
