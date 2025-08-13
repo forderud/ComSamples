5a50f18581cbc18e60d341444d263a0024712846
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-08-13 15:34:42 +0200

    Call ResolveOxid to resolve local TCP port from OXID.

--- MyClientCpp/Main.cpp
+++ MyClientCpp/Main.cpp
@@ -25,6 +25,62 @@ std::vector<T> ToStdVector(const SAFEARRAY * sa) {
 }
 
 
+inline HRESULT GetTcpPortFromOXID(OXID oxid, uint16_t& port) {
+    RPC_BINDING_HANDLE rpcConn = 0;
+    {
+        // RPC over TCP/IP MUST use port 135 (https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-rpce/95fbfb56-d67a-47df-900c-e263d6031f22)
+        RPC_WSTR rpcConnStr = nullptr;
+        RPC_STATUS status = RpcStringBindingComposeW(nullptr, RPC_WSTR(L"ncacn_ip_tcp"), RPC_WSTR(L"127.0.0.1"), RPC_WSTR(L"135"), NULL, &rpcConnStr);
+        if (status != RPC_S_OK)
+            return E_FAIL;
+
+        status = RpcBindingFromStringBindingW(rpcConnStr, &rpcConn);
+        if (status != RPC_S_OK)
+            return E_FAIL;
+
+        RpcStringFreeW(&rpcConnStr);
+    }
+
+    // Make OXID Resolver authenticate without a password
+    RPC_STATUS status = RpcBindingSetAuthInfoExW(rpcConn,
+        RPC_WSTR(L"NT Authority\\NetworkService"),
+        RPC_C_AUTHN_LEVEL_PKT_INTEGRITY, // see https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-dcom/9b20084f-f673-4486-b81c-f6cdcc5edf84
+        RPC_C_AUTHN_DEFAULT,
+        NULL,
+        RPC_C_AUTHZ_NONE,
+        nullptr
+    );
+    if (status != RPC_S_OK)
+        return E_FAIL;
+
+    // Query OXID Resolver for COM server TCP port
+    // This will cause the COM server to start listening on the returned TCP port
+    unsigned short requestedProtocols[] = { 0x07 }; // TCP protocol ID from DCE 1.1: Remote Procedure Call
+    DUALSTRINGARRAY* COMServerStringBindings = nullptr;
+    IPID remoteUnknownIPID{};
+    DWORD authHint = 0;
+    error_status_t err = ResolveOxid(rpcConn, &oxid, _countof(requestedProtocols), requestedProtocols, &COMServerStringBindings, &remoteUnknownIPID, &authHint);
+    if (err != RPC_S_OK)
+        return E_FAIL;
+
+    RpcBindingFree(&rpcConn);
+
+    std::wstring_view strBindings((wchar_t*)COMServerStringBindings->aStringArray, COMServerStringBindings->wSecurityOffset);
+    //std::wstring_view secBindings((wchar_t*)COMServerStringBindings->aStringArray + COMServerStringBindings->wSecurityOffset, COMServerStringBindings->wNumEntries - COMServerStringBindings->wSecurityOffset);
+
+    std::wstring localPort; // server TCP port
+    {
+        size_t idx1 = strBindings.find(L"[");
+        size_t idx2 = strBindings.find(L"]", idx1);
+
+        localPort = strBindings.substr(idx1 + 1, idx2 - idx1 - 1);
+    }
+
+    port = (uint16_t)std::stoi(localPort);
+    return S_OK;
+}
+
+
 /** Non-creatable COM class that doesn't need any CLSID. */
 class MyClient : 
     public CComObjectRootEx<CComMultiThreadModel>, // also compatible with STA
@@ -93,6 +149,14 @@ int main() {
             _com_error err(hr);
             std::wcout << L"CoCreateInstance failure: " << err.ErrorMessage() << std::endl;
             return 1;
+        }
+
+        {
+            COGETSERVERPID_STDOBJREF objref{};
+            GetServerSTDOBJREF(server, /*out*/objref);
+            uint16_t port = 0;
+            GetTcpPortFromOXID(objref.oxid, port);
+            std::wcout << L"Server TCP port: " << port << L"\n";
         }
 
         try {
